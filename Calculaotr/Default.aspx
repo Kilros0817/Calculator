1677ef2c341199ed1415ff8ad97efc259f662576
Kilros0817 <lazarkitfrl@gmail.com>
2022-04-06 08:31:46 +0300

    create asp net web app calculator

---
+++ Calculaotr/Default.aspx
@@ -0,0 +1,57 @@
+﻿<%@ Page Title="Circular economy calculator" Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="Calculaotr.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        #Text1 {
+            width: 153px;
+        }
+        #time {
+            width: 183px;
+        }
+        #time0 {
+            width: 183px;
+        }
+        #time1 {
+            width: 183px;
+        }
+        #time2 {
+            width: 183px;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Label ID="Label1" runat="server" Font-Bold="True" Text="Time horizon"></asp:Label>
+            <br />
+            <input id="time" type="number" style="text-align: right; margin-right: 10px;" runat="server" value="0" /><asp:Label ID="Label2" runat="server" Height="28px" Text="years" Width="35px"></asp:Label>
+            <br />
+
+            <asp:Label ID="Label3" runat="server" Font-Bold="True" Text="Annual sales volume"></asp:Label>
+            <br />
+            <input id="annual" type="number" style="text-align: right; margin-right: 10px; width: 183px;" runat="server" value="0" /><asp:Label ID="Label4" runat="server" Height="28px" Text="units" Width="35px"></asp:Label>
+            <br />
+
+            <asp:Label ID="Label5" runat="server" Font-Bold="True" Text="Growth rate"></asp:Label>
+            <br />
+            <input id="rate" type="number" style="text-align: right; margin-right: 10px; width: 183px;" runat="server" value="0"/><asp:Label ID="Label6" runat="server" Height="25px" Text="%" Width="35px"></asp:Label>
+            <br />
+
+            <asp:Label ID="Label7" runat="server" Font-Bold="True" Text="System effectiveness"></asp:Label>
+            <br />
+            <input id="systemE" type="number" style="text-align: right; margin-right: 10px; width: 183px;" runat="server" value="0"/><asp:Label ID="Label8" runat="server" Height="25px" Text="%" Width="35px"></asp:Label>
+            <br />
+            <br />
+            <asp:Button ID="evaluateBtn" runat="server" Text="Evaluate returns" OnClick="evaluateBtn_Click" />
+            <br />
+        </div>
+        <p>&nbsp;</p>
+    </form>
+    <div id="resultDiv" runat="server">
+    </div>
+    </body>
+</html>
---
+++ Calculaotr/Default.aspx.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Calculaotr
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private int GetCollectionCountNYear(int _n, int _volumn, float _effect)
+        {
+            double percent = (100 - _effect) * _effect / 100 / Math.Pow(_n + 1, 1.75);
+            int volume = Convert.ToInt32(_volumn * percent / 100); 
+            return volume;
+        }
+        private int GetSalesVolumeNYear(int _n, int _volumn, float _rate) 
+        {
+            int volume = Convert.ToInt32(_volumn * Math.Pow(1 + _rate/100, _n));
+            return volume;
+        }
+
+        protected void evaluateBtn_Click(object sender, EventArgs e)
+        {
+            int timeY = int.Parse(time.Value);
+            int volumeY = int.Parse(annual.Value);
+            float growthRate = float.Parse(rate.Value);
+            float effect = float.Parse(systemE.Value);
+            int total = 0;
+            int totalN = 0;
+            string symbol = string.Empty;
+            LiteralControl literalControl = new LiteralControl();
+
+            for (int i = 0; i < timeY; i++)
+            {
+                int volumeNY = GetSalesVolumeNYear(i, volumeY, growthRate); //calculate sales volume of year N 
+                totalN = Convert.ToInt32(volumeNY * effect / 100);
+                string result = string.Empty;
+                result = $"Return volumn from Year {i + 1} sales: {totalN} + ";
+
+                if (i == timeY - 1)
+                    result = $"Return volumn from Year {i + 1} sales: <b>{totalN}</b>";
+
+                for (int j = 1; j < timeY - i; j++)
+                {
+                    int collect = GetCollectionCountNYear(j, volumeNY, effect); //calculate number of collections in year N 
+                    symbol = j == (timeY - i - 1) ? "" : "+";
+                    result += $"{collect} {symbol} ";
+                    totalN += collect;
+                    if (j == timeY - i - 1)
+                        result += $"= <b>{totalN}</b><br>";
+                }
+
+                literalControl = new LiteralControl(result);
+                resultDiv.Controls.Add(literalControl);
+                total += totalN;
+            }
+            literalControl = new LiteralControl($"<br><b>Total:</b> {total} units");
+            resultDiv.Controls.Add(literalControl);
+        }
+    }
+}
